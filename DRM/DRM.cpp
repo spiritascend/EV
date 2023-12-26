95b8f7447f450288c6cec3e79255dd0536be7e1b
Joe <25163821+spiritascend@users.noreply.github.com>
2023-12-26 06:10:03 +0000

    Merry Late Christmas

---
+++ DRM/DRM.cpp
@@ -0,0 +1,83 @@
+#include "FNF/containers.h"
+#include "lib/aes.hpp"
+#include "FNF/utils.h"
+
+
+
+/*
+This project was more for educational purposes only. This is not to be used maliciously or to pirate content. I made this purely because I was bored and had nothing else to do.
+Thank you samuel (@samuels1v on twitter) for assisting me in decompiling the function (It made this a lot quicker).
+*/
+
+
+void* DecipherKey(__int64 a1, int a2, int a3) {
+
+	auto v3 = *(uint64_t*)(a1 + 8);
+	auto v5 = (int)a3;
+	auto v6 = a2;
+	*(uint64_t*)(a1 + 8) = v3 + a3;
+	if (a3 > *(__int64*)(a1 + 0xC) - v3)
+		*(uint64_t*)(a1 + 8) = (uint64_t)_aligned_realloc(&v3, 8, 1);
+
+
+	auto result = memmove((void*)(*(__int64*)a1 + v6 + v5), (const void*)(*(__int64*)a1 + v6), (unsigned int)(v3 - v6));
+	return result;
+}
+
+
+std::string DecryptEnvelope(std::string EvString, std::string Bearer) {
+	std::vector<unsigned char> Envelope = decodeBase64(EvString);
+
+	if (Envelope[0] == 1) {
+		auto fourthbyteofEnv = Envelope[3];
+		auto fifthbyteofEnv = Envelope[4];
+
+		Envelope.erase(Envelope.begin(), Envelope.begin() + 5);
+
+		auto EnvelopeSizeAfter5ByteRemoval = Envelope.size();
+
+		if (Bearer.size() >= fourthbyteofEnv) {
+			auto subkey = Bearer.substr(Bearer.length() - fourthbyteofEnv);
+
+			if (subkey.size() == fourthbyteofEnv) {
+				auto TrailBytes = (char*)Envelope.data() + (int)Envelope.size() - (0x10 - fourthbyteofEnv);
+
+				auto Key = Conv_StringToCharArray(subkey);
+				DecipherKey((__int64)&Key, 0, 0x10 - fourthbyteofEnv);
+
+
+				if (0x10 != (uint64_t)fourthbyteofEnv)
+					memmove(Key.Data, TrailBytes, (0x10 - fourthbyteofEnv));
+
+				Envelope.erase(Envelope.begin() + (fourthbyteofEnv + EnvelopeSizeAfter5ByteRemoval - 0x10), Envelope.begin() + ((fourthbyteofEnv + EnvelopeSizeAfter5ByteRemoval - 0x10) + (0x10 - fourthbyteofEnv)));
+
+
+
+				if (!(((int)Envelope.size() - (int)fifthbyteofEnv) % 16)) {
+					struct AES_ctx ctx;
+
+					uint8_t IV[16] = { 0 };
+					uint8_t* encryptedintarray = reinterpret_cast<uint8_t*>(Envelope.data() + fifthbyteofEnv);
+
+
+					AES_init_ctx_iv(&ctx, (const uint8_t*)Key.Data, IV);
+					AES_CBC_decrypt_buffer(&ctx, encryptedintarray, Envelope.size() - fifthbyteofEnv);
+
+
+					return ExtractObjectFromBuff(std::string((const char*)encryptedintarray));
+				}
+
+			}
+		}
+	}
+}
+
+int main()
+{
+	std::string EvString = "ENVELOPE_STRING";
+	std::string Bearer = "BEARER_GOES_HERE"; // This includes the "bearer eg1~..."
+
+	std::string DecryptedString = DecryptEnvelope(EvString, Bearer);
+
+	std::cout << DecryptedString << std::endl;
+}
---
+++ DRM/FNF/containers.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <boost/archive/iterators/transform_width.hpp>
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <algorithm> 
+
+template<class T>
+struct TArray
+{
+	friend struct FString;
+
+public:
+	T* Data;
+	int32_t Count;
+	int32_t Max;
+
+	inline TArray()
+	{
+		Data = nullptr;
+		Count = Max = 0;
+	};
+
+	inline int Num() const
+	{
+		return Count;
+	};
+
+	inline T& operator[](int i)
+	{
+		return Data[i];
+	};
+
+	inline const T& operator[](int i) const
+	{
+		return Data[i];
+	};
+
+	inline bool IsValidIndex(int i) const
+	{
+		return i < Num();
+	}
+
+
+	inline void Add(T InputData)
+	{
+		Data = (T*)realloc(Data, sizeof(T) * (Count + 1));
+		Data[Count++] = InputData;
+		Max = Count;
+	};
+
+	inline void Remove(int32_t Index)
+	{
+		TArray<T> NewArray;
+		for (size_t i = 0; i < this->Count; ++i)
+		{
+			if (i == Index)
+				continue;
+
+			NewArray.Add(this->operator[](i));
+		}
+		this->Data = (T*)realloc(NewArray.Data, sizeof(T) * (NewArray.Count));
+		this->Count = NewArray.Count;
+		this->Max = NewArray.Count;
+	}
+
+
+
+	inline void Remove(T InData)
+	{
+		TArray<T> NewArray;
+		for (size_t i = 0; i < this->Count; ++i)
+		{
+			if (this->operator[](i) != InData)
+			{
+				NewArray.Add(this->operator[](i));
+			}
+		}
+		this = NewArray;
+	}
+
+	inline bool IsValid()
+	{
+		return Data != nullptr;
+	}
+
+};
+
